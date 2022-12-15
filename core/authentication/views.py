c2431de32b363723e49f8f71bd95c5a7a0235a3f
Iran Mederos <iranmederos@gmail.com>
2022-12-15 19:32:44 -0300

    retorno json user

--- core/authentication/views.py
+++ core/authentication/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from rest_framework.decorators import api_view
 from .serializers import UserSerializer
 from .messages.responses_ok import LOGIN_OK, SIGNUP_OK
 from .messages.responses_error import LOGIN_CREDENTIALS_REQUIRED_ERROR 
@@ -16,20 +16,21 @@ class LoginView(APIView):
 
     def post(self, request):
         # Recuperamos las credenciales y autenticamos al usuario
-        user = request.data.get('username', None)
+        username = request.data.get('username', None)
         password = request.data.get('password', None)
-       
-        if user is None or password is None:
-            return Response(LOGIN_CREDENTIALS_REQUIRED_ERROR, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            user = authenticate(user = user, password = password)
-            if user is not None:
-                return Response( {
-                "user": UserSerializer(user, context = self.get_serializer_context()).data,
-                "message": LOGIN_OK
+        user = authenticate(username=username, password=password)
+
+        # Si es correcto añadimos a la request la información de sesión
+        if user:
+            login(request, user)
+            return Response({
+            "user": UserSerializer(user).data,
+            "message": LOGIN_OK
             }, status=status.HTTP_200_OK)
-            else:
-                return Response(LOGIN_CREDENTIALS_REQUIRED_ERROR, status=status.HTTP_401_UNAUTHORIZED)
+
+        # Si no es correcto devolvemos un error en la petición
+        return Response(
+            status=status.HTTP_404_NOT_FOUND)
 
 
 class LogoutView(APIView):
@@ -39,3 +40,8 @@ class LogoutView(APIView):
 
         # Devolvemos la respuesta al cliente
         return Response(status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def mensaje(request):
+     return Response({"msg":"Hola Mundo"})
